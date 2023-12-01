06a5fe3337e96a541f19ed6e7c5fc07f4bdf8da9
Zach Willis <zachmw27@gmail.com>
2023-11-30 23:18:55 -0500

    First commit

---
+++ README.txt
@@ -0,0 +1,35 @@
+#PHASE 1 PROJECT
+
+#INTRO
+* This is a project that goes over a few different topics and ideas that were 
+    taught in phase 1 of the cirriculum.
+* This application has a few different features.
+    * I have included a few different event listeners
+    * I have also incoorperated a few different API's into the application as well
+
+#EVENT LISTENERS
+* There is also a submit event in which there is a comment box and a submit button.
+    Whenever the user writes a comment and hits the submit button, it submits it to the "server"
+* The event listeners include a click event in which there is a surprise button, and 
+    whenever that button is pressed, a photo of a dog will appear at random.
+* I have included a keydown event listener. Whenever the user presses either the left 
+    or right arrow key on their keyboard, the computer will refresh a table of data and 
+    shuffle through different information
+
+#API'S
+* This application taps into a few different API's
+* One API is the dog CEO website. Any time that the "surprise button" is clicked,
+    it will access that API and randomly display a photo of a dog
+* The next API used is from Cheap Shark. In this, Javascript fetches data from the API and 
+displays it in a table. Once the user clicks either the left or right keys on the keyboard,
+it will update the table and display new data.
+* The last API used is from balldontlie.io. In this, the API is accessed and displays another
+table of data. However, this table only shows players with the first name Jalen. Their player ID,
+first name, and last name are displayed in a table.
+
+#RESOURCES
+* API URL'S
+    * "https://dog.ceo/api/breeds/image/random"
+    * `https://www.cheapshark.com/api/1.0/deals?storeID=${storeID}&upperPrice=${upperPrice}`
+    * "https://www.balldontlie.io/api/v1/players"
+* All photos used are from https://dog.ceo/api/breeds/image/random
---
+++ index.js
@@ -0,0 +1,216 @@
+// Function that displays a random photo from the API whenever the button is clicked
+document.addEventListener('DOMContentLoaded', function () {
+    //Declaring variabels for the button, div, and pictures
+    const surpriseButton = document.getElementById('surpriseButton');
+    const dayImprover = document.getElementById('dayImprover');
+    const dogPic = document.createElement('img');
+
+    // Function that grabs the photos from the API
+    async function getRandomPic() {
+        // Tries to get photo from API
+        try {
+            const getDogPic = await fetch("https://dog.ceo/api/breeds/image/random");
+            const picInfo = await getDogPic.json();
+            return picInfo.message;
+        // If the fetch request is unsuccessful, it will throw an error
+        } catch (error) {
+            console.error('Error loading dog photo:', error);
+        }
+    }
+
+    // Function that loads new dog pic whenever the button is pressed again
+    async function newDogPic() {
+        const imageLink = await getRandomPic();
+        dogPic.src = imageLink;
+    }
+
+    // Event listener for when the button is pressed, the new dog photo will appear
+    surpriseButton.addEventListener('click', newDogPic);
+
+    // Appends to the div
+    dayImprover.appendChild(dogPic);
+});
+
+
+
+
+
+
+
+
+
+
+// Event listener where you can comment and submit that comment
+document.addEventListener('DOMContentLoaded', function () {
+    const formOne = document.getElementById('formOne');
+    const textBox = document.getElementById('text');
+    
+    // Event listener so that the comment goes away when the submit button is pressed
+    formOne.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        // Declaring variable for text box. Value property added so it holds the current text
+        const commentText = textBox.value;
+
+        // If statement that takes comment and can put it in an array and clears the comment box after submitted
+        if (commentText !== '') {
+            console.log('New comment:', commentText);
+            textBox.value = '';
+        }
+    });
+});
+
+
+
+
+
+
+
+
+
+
+// Event listener so that when arrow keys are pressed, it shuffles through the table and displays new data
+document.addEventListener('DOMContentLoaded', function () {
+    // Declaring variables
+    const dataTable = document.querySelector('#dealsTable tbody');
+    let storeID = 1;
+    let upperPrice = 25;
+
+    // Function that gets data from storeID and upperPrices
+    function refreshTable() {
+        fetchCheapSharkData(storeID, upperPrice)
+            // Once promise is returned, it will fill the table with data
+            .then(info => {
+                inputDataInTable(info);
+            });
+    }
+
+    // Function that fills the table with data and affects the HTML
+    function inputDataInTable(data) {
+        dataTable.innerHTML = '';
+
+    
+        // Iterates through the data and arranges it into the table
+        data.forEach(deal => {
+            // Declaring variables that add rows
+            const row = dataTable.insertRow();
+            const [description, lowerPrice, upperPrice] = deal.split(', ');
+            // Declaring variables that add cells
+            const descRow = row.insertCell(0);
+            const lowerPriceRow = row.insertCell(1);
+            const upperPriceRow = row.insertCell(2);
+            // Replaces cells with new data once needed
+            descRow.textContent = description.replace('Description: ', '');
+            lowerPriceRow.textContent = lowerPrice.replace('Lower Price: ', '');
+            upperPriceRow.textContent = upperPrice.replace('Upper Price: ', '');
+        });
+    }
+
+    // Function that fetches data from the API using the two parameters and returns the array of strings into the table
+    function fetchCheapSharkData(storeID, upperPrice) {
+        // Declares variable with the value being the API address
+        const apiUrl = `https://www.cheapshark.com/api/1.0/deals?storeID=${storeID}&upperPrice=${upperPrice}`;
+        // Returns the API address and 
+        return fetch(apiUrl)
+            .then(response => {
+                // Checks to see if the response is ok. If not, it throws an error message
+                if (!response.ok) {
+                    throw new Error(`Error: ${response.status} - ${response.statusText}`);
+                }
+                return response.json();
+            })
+            // Uses .map() to form all of the data into a string and returns all of the data as an array
+            .then(dealsData => {
+                return dealsData.map(deal => {
+                    return `Description: ${deal.title}, Lower Price: ${deal.salePrice}, Upper Price: ${upperPrice}`;
+                });
+            })
+            // If any errors occur, it will display an error message
+            .catch(error => {
+                console.error('Error fetching deals information:', error.message);
+            });
+    }
+
+    // Event listener so that when you press the left or right key, it filters through the table
+    document.addEventListener('keydown', function (event) {
+        switch (event.key) {
+            case 'ArrowLeft':
+                storeID = Math.max(1, storeID - 1);
+                break;
+            case 'ArrowRight':
+                storeID += 1;
+                break;
+        }
+        // Calls the function and refreshes the table when the arrows are pressed
+        refreshTable();
+    });
+
+    // Starting spot
+    refreshTable();
+});
+
+
+
+
+
+
+
+
+
+
+// Function that gets the data from the API and makes it into a table
+async function getPlayers() {
+    const bballApi = "https://www.balldontlie.io/api/v1/players";
+    // Declaring variables according to each parameter
+    const ballParam = {
+      page: 1,
+      per_page: 50,
+      search: 'Jalen'
+    };
+    // Telling what parameters to get from the API
+    const apiUrl = new URL(bballApi);
+    apiUrl.search = new URLSearchParams(ballParam).toString();
+
+    // Gets the data from the API, dynamicall changes the HTML, and puts it all in the table
+    try {
+      // Get request from the API
+      const response = await fetch(apiUrl);
+      // If statement that tells you if its ok
+      if (response.ok) {
+        // Goes through the JSON response
+        const data = await response.json();
+        // Gets the data
+        const players = data.data || [];
+        // Makes a table
+        const table = document.createElement('table');
+        table.border = '2';
+        // Makes table headers
+        const headers = table.createTHead();
+        const headerRow = headers.insertRow(0);
+        headerRow.insertCell(0).textContent = 'ID';
+        headerRow.insertCell(1).textContent = 'First Name';
+        headerRow.insertCell(2).textContent = 'Last Name';
+        // Looks for guys named Jalen and puts all of the info in the table
+        players.forEach(player => {
+          if (
+            player.first_name.toLowerCase().includes('jalen') ||
+            player.last_name.toLowerCase().includes('jalen')
+          ) {
+            const row = table.insertRow();
+            row.insertCell(0).textContent = player.id;
+            row.insertCell(1).textContent = player.first_name;
+            row.insertCell(2).textContent = player.last_name;
+          }
+        });
+        // Appends the table to the body
+        document.body.appendChild(table);
+      } else {
+        console.error(`Error: ${response.status}`);
+      }
+    } catch (error) {
+      console.error('An error occurred:', error);
+    }
+  }
+
+  // Calls the function and shows the data
+  getPlayers();
